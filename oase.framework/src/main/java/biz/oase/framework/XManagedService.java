50ccb69b96ee5d7b1e557062be71ffeabd410e27
it-lo <it-lo@WMH-SPECTRE.fritz.box>
2025-02-08 13:08:41 +0100

    Initial Commit

---
+++ oase.framework/src/main/java/biz/oase/framework/XManagedService.java
@@ -0,0 +1,62 @@
+/* --------------------------------------------------------------------------
+ * Project: OASE - Open Application Service Engine Framework
+ * --------------------------------------------------------------------------
+ * Use of this software is subject to license terms. All Rights Reserved.
+ * -------------------------------------------------------------------------- */
+
+package biz.oase.framework;
+
+import java.util.Dictionary;
+
+import org.osgi.service.cm.ConfigurationException;
+import org.osgi.service.cm.ManagedService;
+
+import biz.car.config.ConfigObject;
+import biz.car.config.XConfig;
+
+/**
+ * Base class for implementing an OSGi <code>ManagedService</code>.
+ * 
+ * @version 1.0.0 07.02.2025 15:01:22
+ */
+public abstract class XManagedService
+	extends ConfigObject
+	implements ManagedService {
+
+	/**
+	 * Creates a default <code>ManagedTimerTask</code> instance.
+	 */
+	public XManagedService() {
+		super();
+	}
+
+	/**
+	 * Performs the actions on receiving a configuration when it is registered or
+	 * modified.
+	 * 
+	 * @param aProps the configuration items
+	 * @throws ConfigurationException
+	 */
+	@Override
+	public void updated(Dictionary<String, ?> aProps)
+		throws ConfigurationException {
+		if (aProps == null) {
+			disabledService();
+
+			return;
+		}
+		accept(XConfig.fromDictionary(aProps));
+		updatedService();
+	}
+
+	/**
+	 * Performs the actions on receiving an empty configuration.
+	 */
+	protected abstract void disabledService();
+
+	/**
+	 * Performs the actions on receiving a configuration when it is registered or
+	 * modified.
+	 */
+	protected abstract void updatedService();
+}
---
+++ oase.framework/src/main/java/biz/oase/framework/bundle/MSG.java
@@ -0,0 +1,41 @@
+/* --------------------------------------------------------------------------
+ * Project: OASE - Open Application Service Engine Framework
+ * --------------------------------------------------------------------------
+ * Use of this software is subject to license terms. All Rights Reserved.
+ * -------------------------------------------------------------------------- */
+
+package biz.oase.framework.bundle;
+
+import biz.car.config.BCS;
+
+/**
+ * Framework messages.
+ *
+ * @version 1.0.0 07.02.2025 15:07:45
+ */
+public class MSG {
+
+	public static String CLASS_NOT_FROM_BUNDLE;
+	public static String COMPONENT_ACTIVATED;
+	public static String COMPONENT_ACTIVATING;
+	public static String COMPONENT_ACTIVATION_FAILED;
+	public static String COMPONENT_DEACTIVATED;
+	public static String COMPONENT_DEACTIVATING;
+	public static String TIMER_CANCELLED;
+	public static String TIMER_STARTED;
+	public static String TIMER_STOPPED;
+
+	// -------------------------------------------------------------------------
+	// Initialize the static fields
+	// -------------------------------------------------------------------------
+	static {
+		BCS.initialize(MSG.class, "MSG.properties"); //$NON-NLS-1$
+	}
+
+	/**
+	 * Creates a default <code>MSG</code> instance.
+	 */
+	private MSG() {
+		super();
+	}
+}
