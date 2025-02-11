38c34a3825e18b8fecd46d49b173bb13308a6c9d
it-lo <it-lo@WMH-SPECTRE.fritz.box>
2025-02-11 09:23:01 +0100

    Framework Module rebuilt

---
+++ oase.framework/src/main/java/biz/oase/framework/OASE.java
@@ -0,0 +1,53 @@
+/* --------------------------------------------------------------------------
+ * Project: OASE - Open Application Service Engine Framework
+ * --------------------------------------------------------------------------
+ * Use of this software is subject to license terms. All Rights Reserved.
+ * -------------------------------------------------------------------------- */
+
+package biz.oase.framework;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.dto.BundleDTO;
+
+import biz.car.CAR;
+import biz.oase.framework.bundle.CA;
+import biz.oase.framework.bundle.VAR;
+
+/**
+ * OASE constants.
+ *
+ * @version 1.0.0 04.11.2024 10:04:32
+ */
+public interface OASE extends CAR {
+
+	/**
+	 * A reference to the OSGi Configuration Admin service.
+	 */
+	XConfigAdmin CAS = CA.instance;
+
+	/**
+	 * The version number of the OASE framework.
+	 */
+	String VERSION = getVersion();
+
+	/**
+	 * The name of the ASGi workspace.
+	 */
+	String WORKSPACE = getWorkspace();
+
+	private static String getVersion() {
+		Bundle l_bundle = FrameworkUtil.getBundle(OASE.class);
+		BundleDTO l_dto = l_bundle.adapt(BundleDTO.class);
+
+		return l_dto.version;
+	}
+
+	private static String getWorkspace() {
+		Bundle l_bundle = FrameworkUtil.getBundle(OASE.class);
+		String l_key = VAR.WORKSPACE;
+		String l_ret = l_bundle.getBundleContext().getProperty(l_key);
+
+		return l_ret;
+	}
+}
---
+++ oase.framework/src/main/java/biz/oase/framework/bundle/VAR.java
@@ -0,0 +1,26 @@
+/* --------------------------------------------------------------------------
+ * Project: OASE - Open Application Service Engine Framework
+ * --------------------------------------------------------------------------
+ * Use of this software is subject to license terms. All Rights Reserved.
+ * -------------------------------------------------------------------------- */
+
+package biz.oase.framework.bundle;
+
+import biz.car.config.BCS;
+
+/**
+ * Bundle Constants.
+ *
+ * @version 1.0.0 07.02.2025 15:07:25
+ */
+public class VAR {
+
+	public static String WORKSPACE;
+	
+	// -------------------------------------------------------------------------
+	// Initialize the static fields
+	// -------------------------------------------------------------------------
+	static {
+		BCS.initialize(VAR.class, "VAR.conf"); //$NON-NLS-1$
+	}
+}
